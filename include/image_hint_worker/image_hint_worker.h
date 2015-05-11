beb3575976fe756821d9ae774ce127c588313e2b
Philipp <philipp.schmette@tum.de>
2015-05-11 18:12:11 +0200

    added hints

--- include/image_hint_worker/image_hint_worker.h
+++ include/image_hint_worker/image_hint_worker.h
@@ -4,10 +4,14 @@
 #include <lms/module.h>
 #include <lms/imaging/image.h>
 #include <lms/imaging/graphics.h>
+#include "lms/imaging/find/image_hint.h"
 class ImageHintWorker:public lms::Module{
-    const lms::imaging::Image* target;
-    lms::imaging::Image* debug;
-    lms::imaging::BGRAImageGraphics* debugGraphics;
+    const lms::imaging::Image *target;
+    lms::imaging::Image *debug;
+    lms::imaging::BGRAImageGraphics *debugGraphics;
+    lms::imaging::find::HintContainer *hintContainer;
+
+    lms::imaging::Image *gaussBuffer;
 public:
     bool initialize();
     bool deinitialize();
--- src/image_hint_worker.cpp
+++ src/image_hint_worker.cpp
@@ -3,37 +3,54 @@
 #include <lms/imaging/find/line.h>
 #include <lms/imaging/draw_debug.h>
 #include <lms/imaging/image_factory.h>
+#include <lms/imaging/find/image_hint.h>
 bool ImageHintWorker::cycle(){
-    lms::imaging::Image *gaussBuffer = new lms::imaging::Image();
-    gaussBuffer->resize(target->width(),target->height(),lms::imaging::Format::GREY);
+    if(gaussBuffer->width() < target->width() || gaussBuffer->height() < target->height()){
+        gaussBuffer->resize(target->width(),target->height(),lms::imaging::Format::GREY);
+    }
     gaussBuffer->fill(255);
     debug->resize(target->width(),target->height(),lms::imaging::Format::BGRA);
     debug->fill(0);
-    //try to find one line
-    lms::imaging::find::Line line;
-    lms::imaging::find::Line::LineParam param;
-    param.target = target;
-    param.approxEdge = false;
-    param.gaussBuffer = gaussBuffer;
-    param.lineWidthMax = 10;
-    param.lineWidthMin = 2;
-    param.maxLength = INFINITY;
-    param.searchAngle = 0;
-    param.searchLength = 100;
-    param.x = 240;
-    param.y = 200;
-    param.sobelThreshold = 250;
-    param.stepLengthMin = 5;
-    param.stepLengthMax = 70;
-    line.find(param,*debugGraphics);
+
+
+    for(lms::imaging::find::ImageHintBase *base: hintContainer->hints){
+        base->find(*debugGraphics);
+    }
+
     return true;
 }
 
 bool ImageHintWorker::initialize(){
+
+    gaussBuffer = new lms::imaging::Image();
     //just for testing
     target = datamanager()->readChannel<lms::imaging::Image>(this,"GREY_IMAGE");
     debug = datamanager()->writeChannel<lms::imaging::Image>(this,"DEBUG_IMAGE");
+    hintContainer = datamanager()->writeChannel<lms::imaging::find::HintContainer>(this,"HINTS");
     debugGraphics = new lms::imaging::BGRAImageGraphics(*debug);
+
+    lms::imaging::find::ImageHint<lms::imaging::find::Line> *hint = new lms::imaging::find::ImageHint<lms::imaging::find::Line>();
+    hint->parameter.target = target;
+    hint->parameter.approxEdge = false;
+    hint->parameter.lineWidthMax = 10;
+    hint->parameter.lineWidthMin = 2;
+    hint->parameter.maxLength = INFINITY;
+    hint->parameter.searchAngle = 0;
+    hint->parameter.searchLength = 100;
+    hint->parameter.gaussBuffer = gaussBuffer;
+    hint->parameter.x = 240;
+    hint->parameter.y = 150;
+    hint->parameter.sobelThreshold = 350;
+    hint->parameter.stepLengthMin = 2;
+    hint->parameter.stepLengthMax = 50;
+    //add it
+    hintContainer->add(hint);
+
+    hint = new lms::imaging::find::ImageHint<lms::imaging::find::Line>(*hint);
+    hint->parameter.x = 80;
+    hint->parameter.searchAngle = -M_PI;
+    hintContainer->add(hint);
+
     return true;
 }
 
