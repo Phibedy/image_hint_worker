fa39109a57783c16f1979a554f5ca22e99df9716
phibedy <philipp.schmette@gmx.de>
2015-11-06 12:51:37 +0100

    DRAW_DEBUG_USED fix

--- src/image_hint_worker.cpp
+++ src/image_hint_worker.cpp
@@ -6,7 +6,7 @@
 #include <lms/imaging_detection/image_hint.h>
 #include "lms/imaging/draw_debug.h"
 bool ImageHintWorker::cycle(){
-#ifdef DRAWDEBUG
+#ifdef DRAW_DEBUG_USED
         //clear the debug image
         debug->fill(0);
 #endif
@@ -16,7 +16,7 @@ bool ImageHintWorker::cycle(){
             logger.error("target is null!!!");
             return true;
         }
-#ifdef DRAWDEBUG
+#ifdef DRAW_DEBUG_USED
         //TODO: shitty code, but as it's just for debugging it's ok
         debug->resize(base->getTarget()->width(),base->getTarget()->height(),lms::imaging::Format::BGRA);
         base->find(*debugGraphics);
